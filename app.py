41aadfd1c94f5873dcc06fbb5443da14e7f87caf
Zach Morrissey <morrissz@oregonstate.edu>
2017-08-15 22:11:40 -0700

    Adding in bulma and filling out basic templates

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return "Hello world!"
+    page_data = {
+        "title": "home"
+    }
+    return render_template('home.html', page_data=page_data)
 
 app.run(debug=True)
