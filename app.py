87f107ead1d79b7998bc32d2743f9ebdf2cbee18
Zach Morrissey <morrissz@oregonstate.edu>
2017-08-20 10:24:08 -0700

    Adding in 404 handler

--- app.py
+++ app.py
@@ -6,9 +6,20 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/home')
 def index():
+    """
+    The main route to the homepage. The "/home" link will be used elsewhere around the site to navigate users back home.
+    """
     page_data = {
         "title": "home"
     }
     return render_template('home.html', page_data=page_data)
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+
+# run the app
 app.run(debug=True)
