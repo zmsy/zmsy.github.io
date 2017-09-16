3c08bfa0f2296dc946ba2201e0815e3698e127f3
Zach Morrissey <zachmor05@gmail.com>
2017-09-16 14:39:23 -0700

    Adding in proper tags to handle dict passed as kwargs to render_template

--- app.py
+++ app.py
@@ -10,22 +10,22 @@ def index():
     """
     The main route to the homepage. The "/home" link will be used elsewhere around the site to navigate users back home.
     """
-    page_data = {
+    page_info = {
         "page_title": "home",
         "post_title": "This is an Example Post",
         "post_subtitle": "In which I Will Explain the Entirety of the Universe"
     }
-    return render_template('home.html', **page_data)
+    return render_template('home.html', **page_info)
 
 @app.route('/about')
 def about():
     """
     The route to the about page, where the user can learn about me and find contact information.
     """
-    page_data = {
+    page_info = {
         "page_title": "about"
     }
-    return render_template('about.html', **page_data)
+    return render_template('about.html', **page_info)
 
 
 # error handlers
@@ -34,12 +34,12 @@ def page_not_found(error):
     """
     Return the 404 page for any times where the URL could not be found.
     """
-    page_data = {
-        "title": "404 - Page Not Found",
+    page_info = {
+        "page_title": "404 - Page Not Found",
         "error": "The page you're looking for does not exist",
         "suggestion": "Please check the URL and try again."
     }
-    return render_template('error.html', **page_data), 404
+    return render_template('error.html', **page_info), 404
 
 
 @app.errorhandler(500)
@@ -47,12 +47,12 @@ def exception_handler(error):
     """
     If there's an internal error in the application at any point, serve up the 500 page.
     """
-    page_data = {
-        "title": "500 - Internal Server Error",
+    page_info = {
+        "page_title": "500 - Internal Server Error",
         "error": "Oops! Internal server error.",
         "suggestion": "Give it a little bit and try again."
     }
-    return render_template('error.html', **page_data), 500
+    return render_template('error.html', **page_info), 500
 
 # run the app
 app.run(debug=True, use_reloader=True)
