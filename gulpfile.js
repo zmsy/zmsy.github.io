ee19ff450c49cfbb27356c8a2cf48bdaa10ec4d7
Zach Morrissey <zachmor05@gmail.com>
2017-09-16 16:25:25 -0700

    Minifying css and writing sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var clean_css = require('gulp-clean-css');
+var source_maps = require('gulp-sourcemaps');
 // var pump = require('pump');
 
 // Process bulma variables
 gulp.task('bulma', function() {
     console.log('running sass');
     return gulp.src('./node_modules/bulma/bulma.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('.'));
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest('./static/css/'));
 });
 
 // Process main scss.
 gulp.task('main-css', function() {
     console.log('running sass');
     return gulp.src('./sources/scss/main.scss')
+        .pipe(source_maps.init())
         .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(clean_css())
+        .pipe(source_maps.write('.'))
         .pipe(gulp.dest('./static/css/'));
 });
