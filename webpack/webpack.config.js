6e271be7d4d2dab23d3579188d600bd3d5269507
Zach Morrissey <zachmor05@gmail.com>
2018-03-31 09:21:56 -0700

    update webpack versions and reinstall dependencies

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,54 +1,60 @@
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+    var webpack = require('webpack');
+    var path = require('path');
+    var ExtractTextPlugin = require('extract-text-webpack-plugin');
+    var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-var options = {
-  entry: {
-    'app': './js/main.js',
-    'styles': './scss/main.scss'
-  },
-  output: {
-    path: path.dirname(__dirname) + '/assets/test/gen',
-    filename: '[name].js'
-  },
-  devtool: '#cheap-module-source-map',
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['*', '.js']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
+    var options = {
+      entry: {
+        'app': './js/main.js',
+        'styles': './scss/main.scss'
       },
-      {
-        test: /\.scss$/,
-        include: [
-          path.resolve(__dirname, './node_modules/'),
-          path.resolve(__dirname, './scss/')
-        ],
-        loader: ExtractTextPlugin.extract({
-          use: ['css-loader', 'sass-loader'],
-          fallback: 'style-loader'
+      output: {
+        path: path.dirname(__dirname) + '/assets/test/gen',
+        filename: '[name].js'
+      },
+      devtool: '#cheap-module-source-map',
+      resolve: {
+        modules: ['node_modules'],
+        extensions: ['*', '.js']
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.scss$/,
+            include: [
+              path.resolve(__dirname, './node_modules/'),
+              path.resolve(__dirname, './scss/')
+            ],
+            loader: ExtractTextPlugin.extract({
+              use: ['css-loader', 'sass-loader'],
+              fallback: 'style-loader'
+            })
+          },
+          {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+          },
+          {
+            test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g\|\.gif$/,
+            loader: 'file'
+          }
+        ]
+      },
+      plugins: [
+        new ExtractTextPlugin('styles.css', {
+          allChunks: true
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeCssAssetsPlugin({
+          assetNameRegExp: /\.css$/g,
+          cssProcessor: require('cssnano'),
+          cssProcessorOptions: { discardComments: { removeAll: true } }
         })
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-      },
-      {
-        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g\|\.gif$/,
-        loader: 'file'
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css', {
-      allChunks: true
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
-};
+      ]
+    };
 
-module.exports = options;
+    module.exports = options;
