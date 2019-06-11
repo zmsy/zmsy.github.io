33490221fb25e2d9c6dc5aef270a1c199840da3b
Zach Morrissey <zachmor05@gmail.com>
2019-06-11 08:33:23 -0700

    webpack updates and moving extra files around

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -14,15 +14,13 @@ module.exports = {
     })
   ],
   module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'style-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+    }]
   }
 };
