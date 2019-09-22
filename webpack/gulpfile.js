1f583fb33ba2d6d237a4f64fef7ff0c0e17e58c4
Zach Morrissey <zachmor05@gmail.com>
2019-09-21 17:47:04 -0700

    Remove outdate purifyCss

--- webpack/gulpfile.js
+++ webpack/gulpfile.js
@@ -5,7 +5,6 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var clean_css = require('gulp-clean-css');
 var source_maps = require('gulp-sourcemaps');
-var purify = require('gulp-purify-css');
 var rename = require('gulp-rename');
 
 function bulma() {
@@ -38,15 +37,6 @@ function js() {
         .pipe(gulp.dest('../assets/js/'));
 }
 
-function purifyBulma() {
-    return gulp
-        .src('../assets/css/bulma.css')
-        .pipe(purify(['../assets/js/*.js', './build/**/*.html']))
-        .pipe(gulp.dest('./dist/'));
-}
-
 const build = gulp.series(css, bulma, js);
-const refine = gulp.series(purifyBulma);
 
 exports.default = build;
-exports.refine = refine;
