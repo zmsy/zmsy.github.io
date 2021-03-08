51b3f8cadf83d471c9c247378af1dcf0c5f46ae9
zmsy <zmsy@users.noreply.github.com>
2021-03-08 06:04:31 +0000

    deploy: a585dff4a11b1f14e93e7aa19aef0fb5f6f15224

---
+++ ts/index.js
@@ -0,0 +1,6 @@
+(() => {
+  // <stdin>
+  document.getElementById("navbar-toggle").addEventListener("click", function() {
+    document.getElementsByClassName("navbar-menu")[0].classList.toggle("is-active");
+  });
+})();
