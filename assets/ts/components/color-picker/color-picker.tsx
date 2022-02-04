08c362a5f8d7ee12f3e4902097193f1113b99ad5
Zach Morrissey <zachmor05@gmail.com>
2022-02-03 22:30:58 -0800

    Fix dark mode update, add scribble homepage

--- assets/ts/components/color-picker/color-picker.tsx
+++ assets/ts/components/color-picker/color-picker.tsx
@@ -9,14 +9,17 @@ import { setPaletteCSS } from "./color-util";
 import { defaultPaletteDark, defaultPaletteLight } from "./palette";
 
 export const ColorPicker: FunctionComponent = () => {
-  const [darkModeEnabled, setDarkModeEnabled] = useState(true);
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkModeEnabled((e) => !e);
+  };
+
+  useEffect(() => {
     darkModeEnabled
       ? setPaletteCSS(defaultPaletteDark)
       : setPaletteCSS(defaultPaletteLight);
-  };
+  }, [darkModeEnabled]);
 
   return (
     <button class="button dark-mode-selector" onClick={toggleDarkMode}>
