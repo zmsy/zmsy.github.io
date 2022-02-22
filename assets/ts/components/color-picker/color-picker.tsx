93194f55bad748394283ec1fc44745a6f71ebc18
Zach Morrissey <zachmor05@gmail.com>
2022-02-22 12:46:16 -0800

    Add css query for dark mode

--- assets/ts/components/color-picker/color-picker.tsx
+++ assets/ts/components/color-picker/color-picker.tsx
@@ -8,8 +8,19 @@ import { useEffect, useState } from "preact/hooks";
 import { setPaletteCSS } from "./color-util";
 import { defaultPaletteDark, defaultPaletteLight } from "./palette";
 
+// Check to see if the user has dark mode enabled in CSS. First need
+// to check if the `matchMedia` is defined because otherwise the browser
+// doesn't support dark mode.
+let darkModeCssEnabled = false;
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  darkModeCssEnabled = true;
+}
+
 export const ColorPicker: FunctionComponent = () => {
-  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+  const [darkModeEnabled, setDarkModeEnabled] = useState(darkModeCssEnabled);
 
   const toggleDarkMode = () => {
     setDarkModeEnabled((e) => !e);
