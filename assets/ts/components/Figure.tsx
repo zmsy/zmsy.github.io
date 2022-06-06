9758bb78eb6efaf0fed8c82ab632dfee8e9a0a1f
Zach Morrissey <zachmor05@gmail.com>
2022-06-05 17:41:46 -0700

    Add Figure component for color-changing svgs in jsx

---
+++ assets/ts/components/Figure.tsx
@@ -0,0 +1,35 @@
+/**
+
+*/
+
+import clsx from "clsx";
+import { FunctionComponent } from "preact";
+import preact = require("preact");
+
+type FigureProps = {
+  /** Text source of the SVG file. */
+  svg: string;
+  /** Any additional class name to add to the <figure> */
+  additionalClass?: string;
+};
+
+/**
+ * Figure component is used to inline SVG files and have the outputs
+ * be color-responsive on the website.
+ *
+ * NOTE: There's a small nit issue in Hugo where js can't import .svg files
+ * as text, because there's no defined loader in esbuild. Opened this
+ * issue to resolve, fix when possible:
+ * https://github.com/gohugoio/hugo/issues/9978
+ */
+export const Figure: FunctionComponent<FigureProps> = ({
+  svg,
+  additionalClass,
+}) => {
+  return (
+    <figure
+      class={clsx("color-responsive-svg", additionalClass)}
+      dangerouslySetInnerHTML={{ __html: svg }}
+    />
+  );
+};
--- assets/ts/components/Modal.tsx
+++ assets/ts/components/Modal.tsx
@@ -2,6 +2,14 @@ import clsx from "clsx";
 import { FunctionComponent } from "preact";
 
 import { useStore } from "ts/store";
+import { Figure } from "./Figure";
+
+/** See note in Figure.tsx for why these are .txt files. */
+import blogIcon from "../../img/icons/blog-icon.txt";
+import homeIcon from "../../img/icons/home-icon.txt";
+import aboutIcon from "../../img/icons/about-icon.txt";
+import recipesIcon from "../../img/icons/recipes-icon.txt";
+import hrzSeparator from "../../img/horizontal-separator.txt";
 
 type ModalProps = {
   /** Must have a unique identifier. */
@@ -15,39 +23,39 @@ export const Modal: FunctionComponent<ModalProps> = ({ id, children }) => {
     <div id={id} class={clsx("modal", navOpen && "is-active")}>
       <div class="modal-background" onClick={handleClickAway}></div>
       <div class="modal-content">
-        <img
-          class="modal-horizontal-separator"
-          src="/img/horizontal-separator.svg"
+        <Figure
+          additionalClass="modal-horizontal-separator"
+          svg={hrzSeparator}
         />
         <ul>
           <li>
             <a href="/">
-              <img class="modal-icon" src="/img/icons/home-icon.svg" />
+              <Figure additionalClass="modal-icon" svg={homeIcon} />
               <span>Home</span>
             </a>
           </li>
           <li>
-            <a href="/">
-              <img class="modal-icon" src="/img/icons/blog-icon.svg" />
+            <a href="/blog">
+              <Figure additionalClass="modal-icon" svg={blogIcon} />
               <span>Blog</span>
             </a>
           </li>
           <li>
-            <a href="/">
-              <img class="modal-icon" src="/img/icons/recipes-icon.svg" />
+            <a href="/recipes">
+              <Figure additionalClass="modal-icon" svg={recipesIcon} />
               <span>Recipes</span>
             </a>
           </li>
           <li>
-            <a href="/">
-              <img class="modal-icon" src="/img/icons/about-icon.svg" />
+            <a href="/about">
+              <Figure additionalClass="modal-icon" svg={aboutIcon} />
               <span>About</span>
             </a>
           </li>
         </ul>
-        <img
-          class="modal-horizontal-separator"
-          src="/img/horizontal-separator.svg"
+        <Figure
+          additionalClass="modal-horizontal-separator"
+          svg={hrzSeparator}
         />
       </div>
       <button
