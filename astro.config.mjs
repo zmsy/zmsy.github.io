5e75f947d858dfbdaa703c5e3a3306b44a22411b
Zach Morrissey <zachmor05@gmail.com>
2023-02-15 20:35:01 -0800

    Add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://zmsy.co',
-	output: "static",
-	integrations: [mdx(), sitemap()],
-	server: {
-		// nostalgic for my hugo days i guess
-		port: 1314,
-	},
-	ssr: {
-		noExternal: ['bulma'],
-	},
-	markdown: {
-		syntaxHighlight: 'prism',
-	},
+  site: 'https://zmsy.co',
+  output: "static",
+  integrations: [mdx(), sitemap(), preact()],
+  server: {
+    // nostalgic for my hugo days i guess
+    port: 1314
+  },
+  ssr: {
+    noExternal: ['bulma']
+  },
+  markdown: {
+    syntaxHighlight: 'prism'
+  }
 });
