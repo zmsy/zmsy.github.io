e6d059fe9dec44406fe1bf288f968c0c9b0fea37
Zach Morrissey <zachmor05@gmail.com>
2023-02-20 19:53:36 -0800

    Add rss ts file

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,14 +2,15 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function get(context) {
+export async function get() {
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: 'https://zmsy.co',
     items: posts.map((post) => ({
       ...post.data,
+      pubDate: post.data.publishDate,
       link: `/blog/${post.slug}/`,
     })),
   });
