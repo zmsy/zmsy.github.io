2b31db8216070b9671871b9a33f7808c930507cb
Zach Morrissey <zachmor05@gmail.com>
2023-03-06 22:02:29 -0800

    Add partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import preact from "@astrojs/preact";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zmsy.co",
   output: "static",
-  integrations: [mdx(), sitemap(), preact(), image()],
+  integrations: [mdx(), sitemap(), preact(), image(), partytown()],
   server: {
     // nostalgic for my hugo days i guess
     port: 1314,
