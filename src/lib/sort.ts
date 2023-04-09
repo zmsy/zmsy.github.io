e6f5ae0f43b4387d0ebece4ab0fc7471b129231c
Zach Morrissey <zachmor05@gmail.com>
2023-04-09 14:12:41 -0700

    Reusable content sort

---
+++ src/lib/sort.ts
@@ -0,0 +1,8 @@
+/**
+ * Sorting functions for displaying content.
+ */
+type DatedContent = { data: { publishDate: Date } };
+export const sortPublishDateDesc = <T extends DatedContent>(
+  a: T,
+  b: T
+): number => b.data.publishDate.valueOf() - a.data.publishDate.valueOf();
