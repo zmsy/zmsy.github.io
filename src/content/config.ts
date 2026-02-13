cd06bf469d67b07a2d2ade22c1a5d9555fcb789e
Zach Morrissey <zachmor05@gmail.com>
2026-02-12 21:03:24 -0800

    Recipes section launch! (#195)
    * Create ny-style-pizza.mdx
    
    * Reveal recipes page
    
    * Add a HowToSection for recipe
    
    * Update content config
    
    * Add image placeholders
    
    * Update RecipeCard.astro
    
    * Add toasted milk walnut cookies recipe
    
    * Update toasted-milk-walnut-cookies.mdx
    
    * Update toasted-milk-walnut-cookies.mdx
    
    * Add placeholder image to recipecard
    
    * Autoformat mdx
    
    * Recipe image scaffolding
    
    * Make recipe preview image clickable
    
    * Remove extra <figure> tag from about layout
    
    * Update ny-style-pizza.mdx
    
    * Create README.md
    
    * Update about image to practice Picture element
    
    * Make recipe preview fields work
    
    * Move placeholder into ./src
    
    * Update images to Picture
    
    * Fix recipe preview height issue
    
    * Fix fallback formats
    
    * Fix about fallback format
    
    * Add images for ube daiquiri
    
    * Update toasted-milk-walnut-cookies.mdx
    
    * Bump baking soda back to where it was
    
    * Update toasted-milk-walnut-cookies.mdx
    
    * Duplicated fallback formats
    
    * Use quality 100 for pics
    
    * Add duration parsing + recipe stats
    
    * Smaller title on mobile
    
    * Update toasted-milk-walnut-cookies.mdx
    
    * Adjust mobile sizes on recipe card
    
    * Remove plural from minutes
    
    * Cookie timings
    
    * add shot lists to recipe content
    
    * Update toasted milk cookies
    
    * Add tinyduration back
    
    * Misc wording changes
    
    * Prettier
    
    * Fix lint failure
    
    * Remove oven toasting method
    
    * Update toasted-milk-walnut-cookies.mdx
    
    * Bump node version to 20
    
    * Recipe editing

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, type ImageFunction } from "astro:content";
 
 const blog = defineCollection({
   schema: z.object({
@@ -19,45 +19,66 @@ const blog = defineCollection({
   }),
 });
 
-const durationRegex =
+/**
+ * ISO-8601 Duration format regex.
+ * https://tc39.es/proposal-temporal/docs/duration.html
+ */
+export const durationRegex =
   /^P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?T?(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)?S)?$/;
 
 const isoDuration = z
   .string()
   .regex(durationRegex, "Invalid ISO 8601 duration format");
 
-const recipeFields = z.object({
+export const recipeInstructionsSection = z.object({
+  /** Will be rendered inline as the title of this section. */
   title: z.string(),
-  description: z.string(),
-  publishDate: z.date(),
-  updatedDate: z.date().optional(),
-  image: z.string().optional(),
-  recipeIngredients: z.array(z.string()),
+  /** Render an anchor tag for this section to link it from other posts. */
+  anchor: z.string(),
+  /** List of instructions. No doubly nested recipes allowed! */
   recipeInstructions: z.array(z.string()),
-  recipeYield: z.string(),
-  cookTime: isoDuration,
-  prepTime: isoDuration,
-  totalTime: isoDuration,
-  /** seo keywords */
-  keywords: z.array(z.string()).optional(),
 });
 
-const drinkSchema = recipeFields.extend({
+const recipeFields = (image: ImageFunction) =>
+  z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date(),
+    updatedDate: z.date().optional(),
+    image: image().optional(),
+    recipeIngredients: z.array(z.string()),
+    recipeInstructions: z.array(
+      z.union([z.string(), recipeInstructionsSection]),
+    ),
+    recipeYield: z.string(),
+    cookTime: isoDuration,
+    prepTime: isoDuration,
+    totalTime: isoDuration,
+    /** seo keywords */
+    keywords: z.array(z.string()).optional(),
+  });
+
+const drinkFields = {
   kingdom: z.literal("drink"),
   category: z.union([
     z.literal("cocktails"),
     z.literal("non-alcoholic"),
     z.literal("hot"),
   ]),
-});
+};
 
-const foodSchema = recipeFields.extend({
-  kingdom: z.literal("drink"),
+const foodFields = {
+  kingdom: z.literal("food"),
   category: z.union([z.literal("dessert"), z.literal("breads")]),
-});
+};
 
 const recipes = defineCollection({
-  schema: z.union([foodSchema, drinkSchema]),
+  schema: ({ image }) => {
+    const commonFields = recipeFields(image);
+    const foodSchema = commonFields.extend(foodFields);
+    const drinkSchema = commonFields.extend(drinkFields);
+    return z.union([foodSchema, drinkSchema]);
+  },
 });
 
 export const collections = { blog, recipes };
