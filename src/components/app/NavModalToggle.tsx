90e345a6bc491b41d24ad27a2df395cab49e453a
Zach Morrissey <zachmor05@gmail.com>
2026-02-21 14:06:51 -0800

    Fix burger menu

--- src/components/app/NavModalToggle.tsx
+++ src/components/app/NavModalToggle.tsx
@@ -5,17 +5,20 @@ import { activeModal } from "@src/state";
  * Toggle button in the nav to display the nav modal when clicked.
  */
 export const NavModalToggle: FunctionComponent = () => {
+  const onClick = () => activeModal.set("nav");
+
   return (
-    <span
-      className="navbar-burger"
-      style={{ color: "var(--textColor)", height: "3rem" }}
-      onClick={() => {
-        activeModal.set("nav");
-      }}
+    <a
+      role="button"
+      onClick={onClick}
+      className="navbar-burger burger"
+      aria-label="Open navigation metnu"
+      aria-expanded="false"
     >
-      <span style={"top: calc(50% + 6px);"}></span>
-      <span style={"top: calc(50% + 10px);"}></span>
-      <span style={"top: calc(50% + 14px);"}></span>
-    </span>
+      <span className="burger-span" aria-hidden="true"></span>
+      <span className="burger-span" aria-hidden="true"></span>
+      <span className="burger-span" aria-hidden="true"></span>
+      <span className="burger-span" aria-hidden="true"></span>
+    </a>
   );
 };
